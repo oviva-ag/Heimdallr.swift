c53a17569dd8c7bb3173b1fc3ce41f4b0199f3a3
Merrick Sapsford <91736021+merricksapsford-oviva@users.noreply.github.com>
2023-01-10 14:22:10 +0100

    APPIOS-2553: Add Swift Package Manager support (#5)
    * Add Package.swift
    
    * Add sources
    
    * Update config
    
    * Bump version to 3.8.0

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 5.7
+
+import PackageDescription
+
+let package = Package(
+    name: "Heimdallr",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .library(
+            name: "Heimdallr",
+            targets: ["Heimdallr"]),
+    ],
+    targets: [
+        .target(
+            name: "Heimdallr",
+            path: "./Heimdallr",
+            exclude: ["Supporting Files/Heimdallr.h", "Supporting Files/Info.plist"],
+            linkerSettings: [
+                .linkedFramework("Foundation")
+            ]),
+    ]
+)
+
